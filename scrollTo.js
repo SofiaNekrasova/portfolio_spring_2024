8be349328dbe3869bde265990e54bff17847a852
Sofi <sofia.nekrasova@gmail.com>
2024-04-05 08:41:57 +0300

    Портфолио

---
+++ scrollTo.js
@@ -0,0 +1,11 @@
+function scrollToFooter() {
+    document.getElementById("footer").scrollIntoView();
+}
+
+function scrollToProjects() {
+    document.getElementById("projects").scrollIntoView();
+}
+
+function scrollToTop() {
+      document.documentElement.scrollTop = 0; 
+}
